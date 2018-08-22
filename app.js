8dd9fc5b367cd06113436f51b9727ede8f1d9881
Bobby Jr Corpus <bobby@corpus.ph>
2018-08-23 05:17:04 +0800

    removing header

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var proxy_secret_token = process.env.PROXY_SECRET_TOKEN
+//var proxy_secret_token = process.env.PROXY_SECRET_TOKEN
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(function(req,res,next){
+/*app.use(function(req,res,next){
  value = req.get('X-3scale-proxy-secret-token');
  if(value != proxy_secret_token){
    res.send(401);
@@ -32,6 +32,7 @@ app.use(function(req,res,next){
  }
  next()
 });
+*/
 
 app.use('/', index);
 app.use('/users', users);
